a004c4f4f9423381f39afb87e2dd57701739756b
Alena <shob@mail.ru>
2023-03-10 18:37:08 +0300

    сортировка подсчетом

---
+++ CountingSort/Program.cs
@@ -0,0 +1,55 @@
+﻿int[] array = {1001, 100, 1001, 1002};
+int[] sortedArray = CountingSortExtended(array);
+//CountingSort(array);
+
+Console.WriteLine(string.Join(", ", sortedArray));
+
+void CountingSort(int[] inputArray)
+{
+    int[] counters = new int[10];
+
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        //counters[inputArray[i]]++;
+        int ourNumber = inputArray[i];
+        counters[ourNumber]++;
+    }
+
+    int index = 0;
+    for (int i = 0; i < counters.Length; i++)
+    {
+        for (int j = 0; j < counters[i]; j++)
+        {
+            inputArray[index] = i;
+            index++;
+        }
+    }
+}
+
+int[] CountingSortExtended(int[] inputArray)
+{
+    int max = inputArray.Max();
+    int min = inputArray.Min();
+
+    int offset = -min;
+    int[] sortedArray = new int[inputArray.Length];
+    int[]counters = new int[max + offset + 1];
+
+    
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        counters[inputArray[i] + offset]++;
+    }
+
+    
+    int index = 0;
+    for (int i = 0; i < counters.Length; i++)
+    {
+        for (int j = 0; j < counters[i]; j++)
+        {
+            sortedArray[index] = i - offset;
+            index++;
+        }
+    }
+    return sortedArray;
+}
